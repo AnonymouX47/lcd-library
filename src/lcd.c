6dc48f970bd8f0c04117fdf82f7e17fa71cfd555
AnonymouX47 <feyidab01@gmail.com>
2020-11-27 00:04:07 +0100

    Added Project files Starting with 8-bit interface mode.

---
+++ src/lcd.c
@@ -0,0 +1,22 @@
+
+// PIC16F877A Configuration Bit Settings
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#include <xc.h>
+#define XTAL_FREQ 20000000
+
+#include "lib/lcd-8.h"
+
+void main(void)
+{
+    ;
+}
---
+++ src/lib/lcd-8.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   lcd-8.h
+ * Author: anonymoux47
+ *
+ * Created on 26 November 2020, 23:54
+ */
+
+#ifndef LCD_8_H
+#define	LCD_8_H
+
+
+
+#endif	/* LCD_8_H */
+
